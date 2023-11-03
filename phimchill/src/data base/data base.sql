473162e6b80a697a3768fe7002ee7ae1314a1281
DESKTOP-LD0AL7S\Thinkpad T470S <nguyenvuthanhnamk24@gmail.com>
2023-11-03 15:59:56 +0700

    fixing create data base

--- phimchill/src/data base/data base.sql
+++ phimchill/src/data base/data base.sql
@@ -1,79 +1,80 @@
-create database phimchill;
-use phimchill;
+create database phimchill5;
+use phimchill5;
 
-CREATE TABLE Genres (
-    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Category  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Movies (
-    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     release_date DATE,
     duration INT,
     director VARCHAR(255),
     genre_id INT,
-    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
+    FOREIGN KEY (genre_id) REFERENCES Category(id)
 );
 
 
 CREATE TABLE Actors (
-    actor_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     birthdate DATE,
     nationality VARCHAR(50)
 );
 
 CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    phone int,
     email VARCHAR(100) NOT NULL
 );
 CREATE TABLE Ratings (
-    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     movie_id INT,
     user_id INT,
     rating DECIMAL(3, 2) NOT NULL,
     review TEXT,
-    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (movie_id) REFERENCES Movies(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE TABLE Comments (
-    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     movie_id INT,
     user_id INT,
     comment_text TEXT,
     comment_date DATETIME,
-    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (movie_id) REFERENCES Movies(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 
 
 CREATE TABLE ViewingHistory (
-    viewing_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     movie_id INT,
     view_date DATETIME,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(id)
 );
 
 CREATE TABLE Watchlist (
-    watchlist_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     movie_id INT,
     date_added DATETIME,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(id)
 );
 
 CREATE TABLE MovieRequests (
-    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     movie_title VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
