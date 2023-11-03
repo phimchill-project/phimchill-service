ff6c0c3d5e22d39d4d9acb56e90131e84ded819b
TaTrap <itistraper@gmail.com>
2023-11-03 13:59:19 +0700

    Structure BE project

---
+++ phimchill/src/main/java/com/codegym/phimchill/PhimchillApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.phimchill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhimchillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhimchillApplication.class, args);
+    }
+
+}
