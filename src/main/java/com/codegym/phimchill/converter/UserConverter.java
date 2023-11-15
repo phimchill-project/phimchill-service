c94b799bdfdae4ee4b64c73079cf38955c2db458
canhheo2002 <tramphattai9624@gmail.com>
2023-11-15 14:10:34 +0700

    Merge branch 'dev' of https://github.com/phimchill-project/phimchill-service into tai-tram
    # Conflicts:
    #	src/main/java/com/codegym/phimchill/controller/CategoryController.java
    #	src/main/java/com/codegym/phimchill/converter/UserConverter.java
    #	src/main/java/com/codegym/phimchill/dto/payload/response/MovieResponse.java

--- src/main/java/com/codegym/phimchill/converter/UserConverter.java
+++ src/main/java/com/codegym/phimchill/converter/UserConverter.java
@@ -3,7 +3,9 @@ package com.codegym.phimchill.converter;
 import com.codegym.phimchill.dto.UserDTO;
 import com.codegym.phimchill.dto.payload.response.RegisterResponse;
 import com.codegym.phimchill.entity.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserConverter {
     UserDTO converterToDTO(User user);
     RegisterResponse converterRegister(User user);
