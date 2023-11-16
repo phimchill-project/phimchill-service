7ad010fd9e001bb3047d06587785dc3675d5aac0
bmhai2504 <bmhai2504@gmail.com>
2023-11-16 13:56:27 +0700

    refactor structure BE

---
+++ src/main/java/com/codegym/phimchill/converter/impl/EpisodeConverterImpl.java
@@ -0,0 +1,8 @@
+package com.codegym.phimchill.converter.impl;
+
+import com.codegym.phimchill.converter.EpisodeConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EpisodeConverterImpl implements EpisodeConverter {
+}
