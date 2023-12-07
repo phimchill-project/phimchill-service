567790c402ae532e5fa9a494aa558d68d05fcdce
tho-nguyen <laotholtt05012002@gmail.com>
2023-12-07 14:27:02 +0700

    Add movie history codes

--- src/main/java/com/codegym/phimchill/converter/impl/MovieHistoryConverterImpl.java
+++ src/main/java/com/codegym/phimchill/converter/impl/MovieHistoryConverterImpl.java
@@ -2,8 +2,12 @@ package com.codegym.phimchill.converter.impl;
 
 import com.codegym.phimchill.converter.MovieHistoryConverter;
 import com.codegym.phimchill.dto.MovieHistoryDto;
+import com.codegym.phimchill.dto.MovieHistoryWithMovieDetailDto;
 import com.codegym.phimchill.entity.MovieHistory;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MovieHistoryConverterImpl implements MovieHistoryConverter {
@@ -15,5 +19,14 @@ public class MovieHistoryConverterImpl implements MovieHistoryConverter {
                 .id(movieHistory.getId())
                 .duration(movieHistory.getDuration())
                 .build();
+    }
+
+    @Override
+    public List<MovieHistoryDto> convertToDtoList(List<MovieHistory> movieHistories) {
+        List<MovieHistoryDto> movieHistoryDtoList = new ArrayList<>();
+        for (MovieHistory movieHistory : movieHistories ) {
+            movieHistoryDtoList.add(convertToDto(movieHistory));
+        }
+        return movieHistoryDtoList;
     }
 }
--- src/main/java/com/codegym/phimchill/repository/MovieHistoryRepository.java
+++ src/main/java/com/codegym/phimchill/repository/MovieHistoryRepository.java
@@ -4,7 +4,11 @@ package com.codegym.phimchill.repository;
 import com.codegym.phimchill.entity.MovieHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieHistoryRepository extends JpaRepository<MovieHistory, Long> {
     MovieHistory findMovieHistoriesByMovie_IdAndAndUser_Id(Long movieId, Long UserId);
+
+    List<MovieHistory> findMovieHistoriesByUser_Id(Long id);
 }
 
